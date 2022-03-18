40dde22177fa1adc0fe3a96b896818c50bde35bf
Israel Ruano <israel.ruano@gmail.com>
2022-03-18 18:41:53 -0300

    subindo arquivos para repositório

---
+++ README.md
@@ -0,0 +1,20 @@
+﻿# Consula API  - CEP + Temperatura
+
+Objetivo: consumir informações de uma API.
+
+Recursos utilizados: 
+
+> API Via CEP
+
+Para obter as informações necessário ao preenchimento do formulário.
+
+> API Tempo
+
+A partir do resultado da consulta ao CEP se obtem a cidade a ser consultada a API Tempo para apresentar a temperatura atual.
+
+> Validação 
+
+Utilizado JavaScript para validar preenchimento correto do CEP e retorno de erro.
+
+
+By Israel Ruano
---
+++ javascript.js
@@ -0,0 +1,46 @@
+
+document.getElementById('cep').addEventListener('focusout', () => { // busca valor digitado após evento focusout, usuário sai do campo cep
+    const cep = document.getElementById('cep').value; // atribui o valor do campo input a variavel "cep"
+    const cepValido = (cep) => cep.length == 8 && /^[0-9]+&/.test(cep);
+
+    if (cepValido(cep)){
+    } else {
+        document.getElementById('endereco').value = 'Ops! CEP incorreto!'
+        }
+
+    fetch(`https://viacep.com.br/ws/${cep}/json/`) // "busca" - efetua a busca / requisição externa via API - endereço deve estar com acento inverso (para a esquerda) ` `
+    .then((resposta) => resposta.json()) // transforma a resposta do formato texto para formato Json
+    .then((dados) => {
+    
+    if (dados.hasOwnProperty('erro')){ // trata retorno de erro caso CEP não seja encontrado
+        document.getElementById('endereco').value = 'Ops! CEP não encontrado!' // mensagem a ser exibida no campo "endereço"
+    } else {
+        
+        document.getElementById('endereco').value = dados.logradouro;
+        document.getElementById('bairro').value = dados.bairro;
+        document.getElementById('cidade').value = dados.localidade;
+        document.getElementById('uf').value = dados.uf;
+                
+        fetch(`https://goweather.herokuapp.com/weather/${dados.localidade}`)
+        .then((resposta) => resposta.json())
+        .then((dadostemp) => {
+        document.getElementById('temperatura').value = dadostemp.temperature;
+        //console.log(dadostemp.temperature);
+        //console.log(dados);
+       
+
+        });
+      }
+    
+     });
+    
+  });
+  
+
+function limpar(){
+document.getElementById('formulario').reset();
+
+}
+
+//document.getElementById('endereco').innerText = JSON.stringify(dados.localidade); // apresenta resultado dentro do campo com ID chamado "debug" (palavra JSON deve estar em maiusculo)
+     
